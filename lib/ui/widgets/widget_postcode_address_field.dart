70309ae18730f3795c0788cfa64fe9e65ba86b56
shahiddgk <mshahid_ms@yahoo.com>
2021-09-24 17:31:49 +0500

    postcode
    post code style changed

--- lib/ui/widgets/widget_postcode_address_field.dart
+++ lib/ui/widgets/widget_postcode_address_field.dart
@@ -132,7 +132,7 @@ class _PostCodeAddressTextFeildWidgetState
 extension PostCodeValidator on String {
   bool isValidPostCode() {
     return RegExp(
-            r'^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$')
+            r'^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([AZa-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))[0-9][A-Za-z]{2})$')
         .hasMatch(this);
   }
 }
